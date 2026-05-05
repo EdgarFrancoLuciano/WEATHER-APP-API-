1297d3b3178d8677cd7a176cbdc7ce217c9048b3
EdgarFrancoLuciano <edgarfrancoluciano@gmail.com>
2026-05-05 12:20:20 -0300

    up

--- script.js
+++ script.js
@@ -2,14 +2,26 @@ async function getWeather() {
   const city = document.getElementById("cityInput").value;
 
   const apiKey = "cdfbe878bb92007f7cfbc3297c90fbb6";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  try {
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${apiKey}&units=metric`
+    );
 
-  const res = await fetch(url);
-  const data = await res.json();
+    const data = await res.json();
 
-  document.getElementById("result").innerHTML = `
-    <h2>${data.name}</h2>
-    <p>${data.main.temp}°C</p>
-    <p>${data.weather[0].description}</p>
-  `;
+    console.log(data);
+
+    if (data.cod !== 200) {
+      document.getElementById("result").innerHTML = "City not found";
+      return;
+    }
+
+    document.getElementById("result").innerHTML = `
+      <h3>${data.name}</h3>
+      <p>${data.main.temp}°C</p>
+      <p>${data.weather[0].description}</p>
+    `;
+  } catch (error) {
+    document.getElementById("result").innerHTML = "Error loading data";
+  }
 }
