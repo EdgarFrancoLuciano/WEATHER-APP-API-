415a7e34936236b43128cc06e8987bb727d46713
EdgarFrancoLuciano <edgarfrancoluciano@gmail.com>
2026-05-05 11:51:25 -0300

    carga

---
+++ script.js
@@ -0,0 +1,15 @@
+async function getWeather() {
+  const city = document.getElementById("cityInput").value;
+
+  const apiKey = "TU_API_KEY";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  document.getElementById("result").innerHTML = `
+    <h2>${data.name}</h2>
+    <p>${data.main.temp}°C</p>
+    <p>${data.weather[0].description}</p>
+  `;
+}
